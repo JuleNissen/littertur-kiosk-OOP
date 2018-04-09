e93d1d7755e7cff18c452c72824e7c415472e74c
JuleNissen <36411057+JuleNissen@users.noreply.github.com>
2018-04-09 16:33:00 +0200

    Single_Book
    Might be good enough for now(?)

--- Single_Book.java
+++ Single_Book.java
@@ -10,15 +10,21 @@
 public class Single_Book extends Books
 {
     private boolean isInSeries;
+    private int edition;
     
     /**
      * Constructor for objects of class Single_Book
+     * title worked by super-class.
+     * Calls setAuthor in super-class to add author.
+     * Set isInSeries to 'false', this is a singel book.
      */
     public Single_Book(String title, String author)
     {
         super(title);
         super.setAuthor(author);
         isInSeries = false;
+        edition = 0; //Book does not exist till contructor is done anyway.
+        IncrementEditionNr(); //If you want edition to start at 1 remove this line. 
     }
     
     /**
@@ -29,5 +35,23 @@ public class Single_Book extends Books
     public boolean inSeries()
     {
         return isInSeries;
+    }
+    
+    /**
+     * Return the book edition.
+     * @Return the book edition.
+     */
+    public int printEdition()
+    {
+        return edition;
+    }
+    
+    /**
+     * Increment edition number.
+     */
+    public void IncrementEditionNr()
+    {
+        this.edition ++;
+        this.edition = edition;
     }
 }
